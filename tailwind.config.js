9590f31fa367eba182a7fc575c943c7322b2c046
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-03-13 18:09:01 +0800

    added custom css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     extend: {
       backgroundImage: {
         'button-color': 'linear-gradient(270deg, #EC47FE 0%, #77D0FD 100%)',
-        'button-color-hover': 'linear-gradient(270deg, #77D0FD 0%, #EC47FE 100%)'
+        'button-color-hover': 'linear-gradient(270deg, #77D0FD 0%, #EC47FE 100%)',
+
+        /** Background */
+        'bg-1': 'linear-gradient(60deg, #3FB5FD38 0%, #FB29FF38 100%)',
       }
     },
   },
