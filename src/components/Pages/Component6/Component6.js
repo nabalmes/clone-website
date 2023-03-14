0f3d3ef2e4aef2f29825b93ee433f868e70b46b8
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-03-14 08:15:55 +0800

    added content

--- src/components/Pages/Component6/Component6.js
+++ src/components/Pages/Component6/Component6.js
@@ -3,8 +3,18 @@ import React from "react";
 
 export const Component6 = () => {
     return (
-        <section id="Component6" className="h-full w-full">
-            <h1 className="text-black">Component6</h1>
+        <section id="Component6" className="h-full w-full text-[#050547]">
+             <div className="page relative w-full h-[220px] flex justify-center items-center bg-bg-1">
+                <h1 className="text-6xl font-semibold text-center">Contact Us</h1>
+                {/* <div className="w-full absolute bottom-0">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                        <path fill="#ffffff" fill-opacity="1" d="M0,288L1440,128L1440,320L0,320Z"></path>
+                    </svg>
+                </div> */}
+            </div>
+            <div className="page flex gap-[2.5rem] flex-col mt-[2.5rem]">
+                <h3 className="text-3xl text-center font-semibold px-5">Sent us a message</h3>
+            </div>
         </section>
     )
 }
