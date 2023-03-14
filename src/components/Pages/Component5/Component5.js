7abf1cf71d4b99f01309019041cf6fe642e0f9e2
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-03-14 08:15:29 +0800

    added content

--- src/components/Pages/Component5/Component5.js
+++ src/components/Pages/Component5/Component5.js
@@ -3,8 +3,15 @@ import React from "react";
 
 export const Component5 = () => {
     return (
-        <section id="Component5" className="h-full w-full">
-            <h1 className="text-black">Component5</h1>
+        <section id="Component5" className="h-full w-full text-[#050547]">
+            <div className="page relative w-full h-[220px] flex justify-center items-center bg-bg-1">
+                <h1 className="text-6xl font-semibold text-center">Videos</h1>
+                {/* <div className="w-full absolute bottom-0">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                        <path fill="#ffffff" fill-opacity="1" d="M0,288L1440,128L1440,320L0,320Z"></path>
+                    </svg>
+                </div> */}
+            </div>
         </section>
     )
 }
