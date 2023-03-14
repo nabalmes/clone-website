942d3bff7d6d16a4bc6a361cd168262d253740b8
Neil Adrian Balmes <neilbalmes4200@gmail.com>
2023-03-14 08:13:45 +0800

    added content

--- src/components/Pages/Component2/Component2.js
+++ src/components/Pages/Component2/Component2.js
@@ -3,8 +3,18 @@ import React from "react";
 
 export const Component2 = () => {
     return (
-        <section id="Component2" className="h-full w-full">
-            <h1 className="text-black">Component2</h1>
+        <section id="Component2" className="h-full w-full text-[#050547]">
+            <div className="page relative w-full h-[220px] flex justify-center items-center bg-bg-1">
+                <h1 className="text-6xl font-semibold text-center">Getting Started</h1>
+                {/* <div className="w-full absolute bottom-0">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                        <path fill="#ffffff" fill-opacity="1" d="M0,288L1440,128L1440,320L0,320Z"></path>
+                    </svg>
+                </div> */}
+            </div>
+            <div className="page flex gap-[2.5rem] flex-col mt-[2.5rem]">
+                <h3 className="text-3xl text-center font-semibold px-5">Ready to build your virtual team?</h3>
+            </div>
         </section>
     )
 }
